9d727d6f5814ab46818f5c6adc805c9dd0f3d9d4
Dushyanth Ganesan <dushyanth.ganesan@gmail.com>
2022-03-28 10:52:48 -0400

    add test for trajectory gen

---
+++ src/traj_test.cpp
@@ -0,0 +1,47 @@
+#include  "ros/ros.h"
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <eigen3/Eigen/Dense>
+
+
+class Test {
+  public: 
+  Test(ros::NodeHandle nh) {
+    ros::Publisher currentStatePub = nh.advertise<nav_msgs::Odometry>("/current_state", 1);
+    ros::Publisher goalPosePub = nh.advertise<nav_msgs::Odometry>("/goal", 1);
+    publishCurrent();
+    publishGoal();
+  }
+
+  void publishCurrent() {
+    nav_msgs::Odometry curr;
+    curr.pose.pose.position.x = 0.0; 
+    curr.pose.pose.position.y = 0.0; 
+    curr.pose.pose.position.z = 0.0; 
+    tf2::Quaternion curr_quat;
+    curr_quat.setRPY(0, 0, 0);
+    curr.pose.pose.orientation = tf2::toMsg(curr_quat)
+    currentStatePub.publish(curr);
+  }
+
+  void publishGoal() {
+    nav_msgs::Odometry goal;
+    goal.pose.pose.position.x = 1.0; 
+    goal.pose.pose.position.y = 1.0; 
+    goal.pose.pose.position.z = 2.0; 
+    tf2::Quaternion goal_quat;
+    goal_quat.setRPY(0, 0, M_PI/2);
+    goal.pose.pose.orientation = tf2::toMsg(goal_quat)
+    currentStatePub.publish(goal);
+  }
+};
+
+int main() {
+  ros::init(argc, argv, "trajectory_test_node");
+  ros::NodeHandle nh;
+
+  Test test(nh);
+
+  ros::spinOnce();
+  return 0;
+}
