285f4bd769a9f7f67a8a91c714861341c3f8a497
wilcohe <willcohe@umich.edu>
2022-03-28 13:50:37 -0400

    Restructured ros calls

--- src/pose_estimator_node.cpp
+++ src/pose_estimator_node.cpp
@@ -38,53 +38,110 @@ class PositionEstimator{
   Eigen::Matrix4f glob_pts;  
   ros::Publisher pos_pub; 
   ros::Subscriber detect_sub; 
+  apriltag_ros::AprilTagDetectionArray curr_detect; 
+
+  void singleTagDetect(const apriltag_ros::AprilTagDetection det, Eigen::Vector3f& curr_p, Eigen::Quaternion& curr_o){
+
+    if (det.id.size()  > 1)
+          return; 
+    
+    int id = det.id[0];
+
+    // Eigen::MatrixXf glob = glob_pts.row(i-1).seq(0,3).transpose();
+
+    // Eigen::Quaternion rpy; 
+
+    // rpy.w() = det.pose.orientation.w; 
+    // rpy.x() = det.pose.orientation.x; 
+    // rpy.y() = det.pose.orientation.y;
+    // rpy.z() = det.pose.orientation.z; 
+
+    // Eigen::Matrix3d Rt = rpy.normalized().toRotationMatrix().transpose();
+
+    // Eigen::Matrix4d TCA = Eigen::Matrix4d::Zero(); 
+
+    // TCA.block<3, 3>(0, 0) = Rt;
+
+    // // TCA.block<3,1>(0,3) = -Rt*glob;
+
+    // Eigen::Vector3f pose_c(det.pose.position.x, det.pose.position.y, det.pose.position.z);
+
+    // TCA.block<3,1>(0,3) = -Rt*pose_c;
+
+    // TCA(3,3) = 1;
+
+    // Eigen::Matrix4f TAO = Eigen::Matrix4f::Identity(); 
+
+    // TAO.block<3,1>(0,3) = glob;
+
+    // Eigen::Matrix4f TCO = TAO*TCA;
+
+    // Quaternion
+
+    Eigen::Quaternion rpy; 
+
+    rpy.w() = det.pose.orientation.w; 
+    rpy.x() = det.pose.orientation.x; 
+    rpy.y() = det.pose.orientation.y;
+    rpy.z() = det.pose.orientation.z;
+
+    Eigen::vector3f glob = glob_pts.row(i-1).seq(0,3).transpose();
+    Eigen::Vector3f pose_c(det.pose.position.x, det.pose.position.y, det.pose.position.z);
+
+    curr_p = glob + pose_c;
+    curr_o = rpy;
+
+  }
+
+  void avgPoses(std::vector<Eigen::Vector3d> curr_ps, std::vector<Eigen::Quaternion> curr_os,
+                Eigen::Vector3f& curr_p, Eigen::Quaternion& curr_o){
+
+    int num_p = curr_ps.size();
+    int nump_o = curr_os.size(); 
+
+    if (num_p != num_o){
+      ROS_WARN("Number of positions is not equal to number of orientations.");
+      throw std::runtime_error("Array sizes not equal");
+    }
+
+    for (int i = 0; i < curr_ps.size(); i++){
+
+      curr_p(0) += curr_ps[i](0)/num_p;
+      curr_p(1) += curr_ps[i](1)/num_p;
+      curr_p(2) += curr_ps[i](2)/num_p;
+
+      curr_o.w() += curr_os[i].w/num_o;
+      curr_o.x() += curr_os[i].x/num_o;
+      curr_o.y() += curr_os[i].y/num_o;
+      curr_o.z() += curr_os[i].z/num_o;
+
+    }
+
+  }
+
+
+  void pubPoses(Eigen::Vector3f curr_p, Eigen::Quaternion curr_o){
+
+    geometry_msgs::Pose pose; 
+
+    pose.position.x = curr_p(0);
+    pose.position.y = curr_p(1); 
+    pose.position.z = curr_p(2);
+
+    pose.orientation.w = curr_o.w; 
+    pose.orientation.x = curr_o.x;
+    pose.orientation.y = curr_o.y; 
+    pose.orientation.z = curr_o.z; 
+
+    pose_pub.publish(pose); 
+
+  }
 
   void positionCallback(const apriltag_ros::AprilTagDetectionArray det){
 
     ROS_INFO("Position Called");
 
-   int num = det.detections.size();
-
-   Eigen::Matrix4f camera; 
-
-   if (num < 4){
-    ROS_INFO("Not enough detections.");
-    return ;
-  }
-
-   geometry_msgs::Pose centroid; 
-
-
-   for (int i = 0; i < num; i++){
-
-    if (det.detections[i].id.size() > 0)
-      continue; 
-
-    int id = det.detections[i].id[0] - 1 ; 
-
-    camera(id, 0) = det.detections[i].pose.pose.pose.position.x; 
-    camera(id, 1) = det.detections[i].pose.pose.pose.position.y; 
-    camera(id, 2) = det.detections[i].pose.pose.pose.position.z; 
-    camera(id, 3) = 1.0; 
-
-   }
-
-   ROS_INFO("Initialized Camera Matrix. Solving...");
-
-   Eigen::Matrix4f sol = camera.ldlt().solve(glob_pts); 
-
-   centroid.position.x = sol(3, 0); 
-   centroid.position.y = sol(3, 1); 
-   centroid.position.z = sol(3, 2); 
-
-   ROS_INFO("Generated solution");
-   ROS_INFO("X: %f", sol(3, 0)); 
-   ROS_INFO("Y: %f", sol(3, 1)); 
-   ROS_INFO("Z: %f", sol(3, 2)); 
-
-   pos_pub.publish(centroid);
-
-   ROS_INFO("Published");  
+    curr_detect = det;  
 
   }
 
@@ -98,6 +155,12 @@ int main(int argc, char** argv)
   ros::NodeHandle nh("~");
 
   PositionEstimator pose_estimator(nh);  
+
+  while(ros::ok()){
+
+
+
+  }
 
 
  // ROS_INFO_THROTTLE(1.0, "Pose estimator initialized. "); 
