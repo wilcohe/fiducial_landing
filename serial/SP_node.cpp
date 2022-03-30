efc7b8ab220dc654ddd5b40b77811ab1497c1212
wilcohe <willcohe@umich.edu>
2022-03-30 12:04:38 -0400

    Changed return type

--- serial/SP_node.cpp
+++ serial/SP_node.cpp
@@ -21,29 +21,29 @@ ros::Subscriber<trajectory_msgs::MultiDOFJointTrajectoryPoint> sub("/desired_sta
 
 char *IP = "192.168.6.1";
 
-double* get_pos_c(){
+// double* get_pos_c(){
 
-    double* arr = new double[13]; 
+//     double* arr = new double[13]; 
 
-    return arr; 
+//     return arr; 
 
-}
+// }
 
-double* send_traj(void){
+traj_type send_traj(void){
 
-    double* traj = new double[13];
+    // double* traj = new double[13];
     traj_type next = *next_traj; 
 
-    for (int i = 0; i < 3; i++){
-        traj[i] = next.pos[i]; 
-        traj[i + 3] = next.vel[i]; 
-        traj[i + 10] = next.rates[i]; 
-    }
+    // for (int i = 0; i < 3; i++){
+    //     traj[i] = next.pos[i]; 
+    //     traj[i + 3] = next.vel[i]; 
+    //     traj[i + 10] = next.rates[i]; 
+    // }
 
-    for (int i = 0; i < 4; i++)
-        traj[i + 6] = next.quat[i]; 
+    // for (int i = 0; i < 4; i++)
+    //     traj[i + 6] = next.quat[i]; 
 
-    return traj; 
+    return next; 
 }
 
 
@@ -54,22 +54,22 @@ int main()
     nh.subscribe(sub);
     nh.advertise(statePub);
     while(1){
-        double* pos = get_pos_c(); 
+        // double *pos = get_pos_c();   
         
         // <x, y, z, vx, vy, vz, qw, qx, qy, qz, vr, vp, vy>
-        odom_msg.pose.pose.position.x = pos[0]; 
-        odom_msg.pose.pose.position.y = pos[1]; 
-        odom_msg.pose.pose.position.z = pos[2];
-        odom_msg.twist.twist.linear.x = pos[3];
-        odom_msg.twist.twist.linear.y = pos[4];
-        odom_msg.twist.twist.linear.z = pos[5];
-        odom_msg.pose.pose.orientation.w = pos[6];
-        odom_msg.pose.pose.orientation.x = pos[7];
-        odom_msg.pose.pose.orientation.y = pos[8];
-        odom_msg.pose.pose.orientation.z = pos[9];
-        odom_msg.twist.twist.angular.x = pos[10];
-        odom_msg.twist.twist.angular.y = pos[11];
-        odom_msg.twist.twist.angular.z = pos[12];
+        // odom_msg.pose.pose.position.x = pos[0]; 
+        // odom_msg.pose.pose.position.y = pos[1]; 
+        // odom_msg.pose.pose.position.z = pos[2];
+        // odom_msg.twist.twist.linear.x = pos[3];
+        // odom_msg.twist.twist.linear.y = pos[4];
+        // odom_msg.twist.twist.linear.z = pos[5];
+        // odom_msg.pose.pose.orientation.w = pos[6];
+        // odom_msg.pose.pose.orientation.x = pos[7];
+        // odom_msg.pose.pose.orientation.y = pos[8];
+        // odom_msg.pose.pose.orientation.z = pos[9];
+        // odom_msg.twist.twist.angular.x = pos[10];
+        // odom_msg.twist.twist.angular.y = pos[11];
+        // odom_msg.twist.twist.angular.z = pos[12];
 
        
         //state.data = //something receive from the rc_pilot
