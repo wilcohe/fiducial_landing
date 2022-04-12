2ff454c0d44fad837b0e624a5cfb880207fe7b2f
wilcohe <willcohe@umich.edu>
2022-04-12 19:11:26 -0400

    correct .h file

--- include/SP_node.h
+++ include/SP_node.h
@@ -7,20 +7,18 @@
 typedef struct traj_type{
 	double pos[3];  
 	double vel[3];  
-	double quat[4]; 
+	double quat[4]; // w x y z
 	double rates[3]; 
 } traj_type; 
 
-typdef struct pose{
+typedef struct pose_type{
 	double pos[3]; 
 	double quat[4]; 
-} pose; 
+} pose_type; 
 
  EXTERNC traj_type send_traj(void);
 
- EXTERNC pose send_pose(void); 
-
- EXTERNC bool send_detect(void); 
+ EXTERNC pose_type send_pose(void); 
 
 
  #undef EXTERNC
