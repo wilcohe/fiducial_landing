529e93574e12bbb6d8e7127c79accbab6a357e82
Dushyanth Ganesan <dushyanth.ganesan@gmail.com>
2022-04-14 16:04:04 -0400

    goal publishing state machine

--- src/traj_test.cpp
+++ src/traj_test.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/Pose.h>
 #include <nav_msgs/Odometry.h>
 #include <eigen3/Eigen/Dense>
+#include <eigen_conversions/eigen_msg.h>
 #include <tf/transform_datatypes.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
@@ -9,36 +10,50 @@
 class Test {
   public: 
   Test(ros::NodeHandle nh) {
-    currentStatePub = nh.advertise<nav_msgs::Odometry>("/current_state", 1, true);
+    ros::param::get("/goal_state", state);
+    currentPoseSub = nh.subscribe("/pose_estimator/position", 1, &Test::getCurrent, this);
+    aprilStateSub = nh.subscribe("/april_state", 1, &&TrajGenerator::activate, this);
     goalPosePub = nh.advertise<nav_msgs::Odometry>("/goal", 1, true);
-    publishCurrent();
-    publishGoal();
   }
 
   private:
-  ros::Publisher currentStatePub;
+  ros::Subscriber currentPoseSub;
+  ros::Subscriber aprilStateSub;
   ros::Publisher goalPosePub;
+  Eigen::Affine3d current_pose_;
+  char state;
 
-  void publishCurrent() {
-    nav_msgs::Odometry curr;
-    curr.pose.pose.position.x = 0.0; 
-    curr.pose.pose.position.y = 0.0; 
-    curr.pose.pose.position.z = 0.0; 
-    tf2::Quaternion curr_quat;
-    curr_quat.setRPY(0, 0, 0);
-    curr.pose.pose.orientation = tf2::toMsg(curr_quat);
-    currentStatePub.publish(curr);
-    ROS_INFO("published current");
+  void getCurrent(const nav_msgs::Odometry::ConstPtr& odom) {
+    // store current position in our planner
+    tf::poseMsgToEigen(odom->pose.pose, current_pose_);
+  }
+
+  void activate(std_msgs::Bool state_msg) {
+    if (state_msg.data) {
+      publishGoal();
+    }
   }
 
   void publishGoal() {
     nav_msgs::Odometry goal;
-    goal.pose.pose.position.x = 1.0; 
-    goal.pose.pose.position.y = 1.0; 
-    goal.pose.pose.position.z = 2.0; 
     tf2::Quaternion goal_quat;
-    goal_quat.setRPY(0, 0, M_PI/2);
-    goal.pose.pose.orientation = tf2::toMsg(goal_quat);
+    switch(state) {
+      case 'l': // land
+        goal.pose.pose.position.x = 0.0; 
+        goal.pose.pose.position.y = 0.0; 
+        goal.pose.pose.position.z = 0.0; 
+        goal_quat.setRPY(0, 0, 0);
+        goal.pose.pose.orientation = tf2::toMsg(goal_quat);
+      case 'g': // guide to above landing zone 
+        goal.pose.pose.position.x = 0.0; 
+        goal.pose.pose.position.y = 0.0; 
+        goal.pose.pose.position.z = 0.75; 
+        goal_quat.setRPY(0, 0, 0);
+        goal.pose.pose.orientation = tf2::toMsg(goal_quat);
+      case 'h': // hover in place
+        tf::poseEigenToMsg(current_pose_, goal.pose.pose)
+        goal.pose.pose.position.z = 0.75; 
+    }
     goalPosePub.publish(goal);
     ROS_INFO("published goal");
   }
